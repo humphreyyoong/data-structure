87514944c7e908fdd7dfd6d32789dfeed09ab5bc
wisyoong <wisyoong@gmail.com>
2020-07-30 16:10:25 +0800

    添加了 直接插入排序

---
+++ 第8章/直接插入排序/直接插入排序/SqList.hpp
@@ -0,0 +1,30 @@
+//
+//  SqList.hpp
+//  散列表的查找
+//
+//  Created by 熊智宏 on 2020/7/24.
+//  Copyright © 2020 wisyoong. All rights reserved.
+//
+
+#ifndef SqList_hpp
+#define SqList_hpp
+
+#include "global.h"
+#define MAXSIZE 1000
+typedef int Status;
+typedef int KeyType;
+typedef struct{
+    KeyType key;
+    String name;
+} ElemType;
+typedef struct{
+    ElemType *baseAddress;
+    int length;
+}SqList;
+Status initSqList(SqList &sqList); //初始化线性表
+int insertElem(SqList &sqList,int index,ElemType elem); //在线性表中插入数据元素
+Status printSqList(SqList sqList); //输出线性表
+Status directInsertionSort(SqList &sqList); //直接插入排序
+#endif /* SqList_hpp */
+
+
---
+++ 第8章/直接插入排序/直接插入排序/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  直接插入排序
+//
+//  Created by 熊智宏 on 2020/7/30.
+//  Copyright © 2020 wisyoong. All rights reserved.
+//
+
+#include <iostream>
+#include "SqList.hpp"
+int main(){
+    SqList sqList;
+       initSqList(sqList);
+       
+    //添加数据
+    ElemType elem;
+    elem.key=16;
+    elem.name="张三";
+    insertElem(sqList,1,elem);
+    elem.key=12;
+    elem.name="李四";
+    insertElem(sqList,2,elem);
+    elem.key=14;
+    elem.name="王五";
+    insertElem(sqList,3,elem);
+    elem.key=18;
+    elem.name="丘六";
+    insertElem(sqList,4,elem);
+    elem.key=13;
+    elem.name="李华";
+    insertElem(sqList,5,elem);
+    elem.key=11;
+    elem.name="李花";
+    insertElem(sqList,6,elem);
+    printSqList(sqList);
+    printf("\n");
+
+    directInsertionSort(sqList); //直接插入排序
+    printSqList(sqList);
+    return 0;
+}
